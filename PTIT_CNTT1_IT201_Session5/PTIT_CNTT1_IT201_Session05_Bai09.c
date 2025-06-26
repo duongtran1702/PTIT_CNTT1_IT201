8007958c55c415949af663ea9029d9708e2ba308
duongtram1702 <duong170226@gmail.com>
2025-06-27 02:25:16 +0700

    Session 5

--- PTIT_CNTT1_IT201_Session5/PTIT_CNTT1_IT201_Session05_Bai09.c
+++ PTIT_CNTT1_IT201_Session5/PTIT_CNTT1_IT201_Session05_Bai09.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#define MAX 100
 int cols, rows;
 
 int countPaths(int i, int j)
@@ -8,6 +9,18 @@ int countPaths(int i, int j)
     if (i == rows - 1 && j == cols - 1)
         return 1;
     return countPaths(i + 1, j) + countPaths(i, j + 1);
+}
+int countPathsDp(int rows,int cols){
+    int dp[MAX][MAX]={0};
+    dp[0][0]=1;
+    for(int i=0;i<rows;i++){
+        for(int j=0;j<cols;j++){
+            if(j>0) dp[i][j]+=dp[i][j-1];
+            if(i>0) dp[i][j]+=dp[i-1][j];
+        }
+    }
+    return dp[rows-1][cols-1];
+
 }
 
 int main()
@@ -20,5 +33,6 @@ int main()
         printf("Invalid input: rows and cols must be positive integers.\n");
         return 1;
     }
-    printf("Total number of valid paths from (0,0) to (%d,%d): %d", rows - 1, cols - 1, countPaths(0, 0));
+    printf("Total number of valid paths from (0,0) to (%d,%d): %d\n", rows - 1, cols - 1, countPaths(0, 0));
+    printf("Total number of valid paths from (0,0) to (%d,%d): %d", rows - 1, cols - 1, countPathsDp(rows, cols));
 }
--- PTIT_CNTT1_IT201_Session5/PTIT_CNTT1_IT201_Session05_Bai10.c
+++ PTIT_CNTT1_IT201_Session5/PTIT_CNTT1_IT201_Session05_Bai10.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#define MAX 100
 int cols, rows;
 int colBlock, rowBlock;
 
@@ -8,11 +9,28 @@ int countPathsWithBlock(int i, int j)
         return 0;
     if (i == rows - 1 && j == cols - 1)
         return 1;
-    if (i == rowBlock-1 && j == colBlock-1)
+    if (i == rowBlock - 1 && j == colBlock - 1)
         return 0;
     return countPathsWithBlock(i + 1, j) + countPathsWithBlock(i, j + 1);
 }
-
+int countPathsWithBlockDp(int rows, int cols)
+{
+    int dp[MAX][MAX] = {0};
+    dp[0][0] = 1;
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            if ( i == rowBlock-1 && j == colBlock-1)
+                continue;
+            if (j > 0)
+                dp[i][j] += dp[i][j - 1];
+            if (i > 0)
+                dp[i][j] += dp[i - 1][j];
+        }
+    }
+    return dp[rows - 1][cols - 1];
+}
 int main()
 {
     printf("Enter the number of rows and cols :");
@@ -31,5 +49,6 @@ int main()
         printf("Invalid block position: must be inside the matrix.\n");
         return 1;
     }
-    printf("Total number of valid paths with block from (0,0) to (%d,%d): %d", rows - 1, cols - 1, countPathsWithBlock(0, 0));
+    printf("Total number of valid paths with block from (0,0) to (%d,%d): %d\n", rows - 1, cols - 1, countPathsWithBlock(0, 0));
+    printf("Total number of valid paths with block from (0,0) to (%d,%d): %d", rows - 1, cols - 1, countPathsWithBlockDp(rows,cols));
 }
